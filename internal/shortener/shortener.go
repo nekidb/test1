b411caa53eae00b7e93d9698d3487c942d408016
nekidb <nekidb@gmail.com>
2023-06-05 17:20:25 +0300

    change shortener service structure

--- internal/shortener/shortener.go
+++ internal/shortener/shortener.go
@@ -2,46 +2,89 @@ package shortener
 
 import (
 	"math/rand"
-	"net/url"
 	"strings"
 )
 
-type SimpleShortener struct{}
-
-func (s SimpleShortener) GetShortPath() string {
-	length := 5
-	return generatePath(length)
+type Shortener interface {
+	GetShortPath(srcURL string) (string, error)
+	GetSourceURL(shortPath string) (string, error)
+	DeleteSourceURL(srcURL string) error
+	// GetCustomShortURL(source, custom string) error
 }
 
-func (s SimpleShortener) ValidateURL(str string) (bool, error) {
-	u, err := url.Parse(str)
-	if err != nil {
-		return false, err
-	}
-
-	if u.Scheme == "" || !isGoodHost(u.Host) {
-		return false, nil
-	}
-	return true, nil
+type URLStorage interface {
+	Save(shortPath, srcURL string)
+	GetShortPath(srcURL string) string
+	GetSourceURL(shortPath string) string
+	DeleteSourceURL(srcURL string)
 }
 
-func isGoodHost(host string) bool {
-	if host == "" {
-		return false
-	}
-	if !strings.Contains(host, ".") {
-		return false
-	}
-	return true
+type ShortenerService struct {
+	storage URLStorage
 }
 
+func NewShortenerService(storage URLStorage) (*ShortenerService, error) {
+	return &ShortenerService{
+		storage: storage,
+	}, nil
+}
+
+func (s ShortenerService) GetShortPath(srcURL string) (string, error) {
+	// Check if exists in DB
+	shortPath := s.storage.GetShortPath(srcURL)
+	if shortPath != "" {
+		return shortPath, nil
+	}
+
+	// If not exists in DB, then create new
+	length := 6
+	shortPath = s.generateRandomPath(length)
+
+	s.storage.Save(shortPath, srcURL)
+
+	return shortPath, nil
+}
+
+func (s ShortenerService) GetSourceURL(shortPath string) (string, error) {
+	// Check if shortPath is correct
+
+	// Get from DB if exists. If not, then return empty string
+	srcURL := s.storage.GetSourceURL(shortPath)
+	return srcURL, nil
+}
+
+func (s ShortenerService) DeleteSourceURL(srcURL string) error {
+	s.storage.DeleteSourceURL(srcURL)
+	return nil
+}
+
+//	func (s ShortenerService) ValidateURL(str string) (bool, error) {
+//		u, err := url.Parse(str)
+//		if err != nil {
+//			return false, err
+//		}
+//
+//		if u.Scheme == "" || !isGoodHost(u.Host) {
+//			return false, nil
+//		}
+//		return true, nil
+//	}
+//
+//	func isGoodHost(host string) bool {
+//		if host == "" {
+//			return false
+//		}
+//		if !strings.Contains(host, ".") {
+//			return false
+//		}
+//		return true
+//	}
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func generatePath(n int) string {
+func (s ShortenerService) generateRandomPath(n int) string {
 	var sb strings.Builder
 	sb.Grow(n)
 
-	sb.WriteString("/")
 	for i := 0; i < n; i++ {
 		sb.WriteByte(letters[rand.Intn(len(letters))])
 	}
--- internal/shortener/shortener_test.go
+++ internal/shortener/shortener_test.go
@@ -1,34 +1,114 @@
 package shortener
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
-func TestValidateURL(t *testing.T) {
-	testCases := []struct {
-		url  string
-		want bool
-	}{
-		{"https://xxx.com", true},
-		{"https://xxx.com/yyy", true},
-		{"https://", false},
-		{"xxx.com", false},
-		{"https://xxx", false},
+func TestShortener(t *testing.T) {
+	const (
+		shortPath = "shorted"
+		srcURL    = "htts://github.com"
+	)
+
+	t.Run("writes to DB if source URL not exists", func(t *testing.T) {
+		storage := NewMockStorage()
+		shortener := createShortener(t, storage)
+
+		if _, err := shortener.GetShortPath(srcURL); err != nil {
+			t.Fatal(err)
+		}
+
+		got := len(storage.data)
+		want := 1
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+	t.Run("gets shorted path from DB", func(t *testing.T) {
+		storage := NewMockStorage()
+		storage.Save(shortPath, srcURL)
+		shortener := createShortener(t, storage)
+
+		got, err := shortener.GetShortPath(srcURL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assertString(t, got, shortPath)
+	})
+	t.Run("gets source URL from DB", func(t *testing.T) {
+		storage := NewMockStorage()
+		storage.Save(shortPath, srcURL)
+		shortener := createShortener(t, storage)
+
+		got, err := shortener.GetSourceURL(shortPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assertString(t, got, srcURL)
+	})
+	t.Run("deletes source URL", func(t *testing.T) {
+		storage := NewMockStorage()
+		storage.Save(shortPath, srcURL)
+		shortener := createShortener(t, storage)
+
+		shortener.DeleteSourceURL(srcURL)
+
+		got := len(storage.data)
+		want := 0
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+}
+
+func createShortener(t *testing.T, storage URLStorage) *ShortenerService {
+	t.Helper()
+	shortener, err := NewShortenerService(storage)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	shortener := SimpleShortener{}
+	return shortener
+}
 
-	for _, test := range testCases {
-		t.Run(fmt.Sprintf("validate %s", test.url), func(t *testing.T) {
-			got, err := shortener.ValidateURL(test.url)
-			if err != nil {
-				t.Fatal(err)
-			}
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
 
-			if got != test.want {
-				t.Errorf("got %v, want %v", got, test.want)
-			}
-		})
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+type MockStorage struct {
+	data map[string]string
+}
+
+func NewMockStorage() *MockStorage {
+	return &MockStorage{
+		data: make(map[string]string),
+	}
+}
+
+func (s *MockStorage) Save(str1, str2 string) {
+	s.data[str1] = str2
+}
+
+func (s *MockStorage) GetShortPath(str string) string {
+	for k, v := range s.data {
+		if v == str {
+			return k
+		}
+	}
+
+	return ""
+}
+
+func (s *MockStorage) GetSourceURL(str string) string {
+	return s.data[str]
+}
+
+func (s *MockStorage) DeleteSourceURL(str string) {
+	for k, v := range s.data {
+		if v == str {
+			delete(s.data, k)
+		}
 	}
 }
