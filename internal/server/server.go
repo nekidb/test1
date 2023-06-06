9e85bdafac82c13671b071b94afd307bb0974a02
nekidb <nekidb@gmail.com>
2023-06-06 16:39:15 +0300

    change handling errors

--- internal/server/server.go
+++ internal/server/server.go
@@ -44,9 +44,13 @@ func (s *Server) Serve() error {
 }
 
 func (s *Server) initRouter() {
-	s.router.Get("/s/{short}", s.errorWrapper(s.redirectHandler))
-	s.router.Post("/api/shorten", s.errorWrapper(s.shortenHandler))
-	s.router.NotFound(s.notFoundErrorHandler)
+	s.router.Get("/s/{short}", s.redirectHandler)
+	s.router.Post("/api/shorten", s.shortenHandler)
+
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		s.handleError(w, http.StatusNotFound)
+	}
+	s.router.NotFound(http.HandlerFunc(fn))
 }
 
 type inputData struct {
@@ -57,82 +61,80 @@ type outputData struct {
 	ShortURL string `json:"shortURL"`
 }
 
-func (s *Server) errorWrapper(handler func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := handler(w, r)
-		if err == nil {
-			return
-		}
-
-		switch err {
-		case errService:
-			s.internalErrorHandler(w, r)
-			// 	case errBadRequest:
-			// 		s.badRequestErrorHandler(w, r)
-		case errNotFound:
-			s.notFoundErrorHandler(w, r)
-		default:
-			s.badRequestErrorHandler(w, r)
-		}
-	}
-}
-
-func (s *Server) internalErrorHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-}
-
-func (s *Server) badRequestErrorHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusBadRequest)
-}
-
-func (s *Server) notFoundErrorHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-}
-
-func (s *Server) shortenHandler(w http.ResponseWriter, r *http.Request) error {
+func (s *Server) shortenHandler(w http.ResponseWriter, r *http.Request) {
 	inputData := &inputData{}
 	if err := json.NewDecoder(r.Body).Decode(inputData); err != nil {
-		return errBadRequest
+		s.handleError(w, http.StatusBadRequest)
+		return
 	}
 
 	srcURL := inputData.URL
 
 	err := validateURL(srcURL)
 	if err != nil {
-		return errBadRequest
+		s.handleError(w, http.StatusBadRequest)
+		return
 	}
 
 	shortPath, err := s.shortener.GetShortPath(srcURL)
 	if err != nil {
-		return errService
+		s.handleError(w, http.StatusInternalServerError)
+		return
 	}
 
 	shortURL := path.Join(s.host+s.port, "s", shortPath)
 
 	output, err := makeOutputJSON(shortURL)
 	if err != nil {
-		return errService
+		s.handleError(w, http.StatusInternalServerError)
+		return
 	}
 
 	w.Write(output)
-
-	return nil
 }
 
-func (s *Server) redirectHandler(w http.ResponseWriter, r *http.Request) error {
+func (s *Server) redirectHandler(w http.ResponseWriter, r *http.Request) {
 	shortPath := chi.URLParam(r, "short")
 
 	srcURL, err := s.shortener.GetSourceURL(shortPath)
 	if err != nil {
-		return errService
+		s.handleError(w, http.StatusInternalServerError)
+		return
 	}
 
 	if srcURL == "" {
-		return errNotFound
+		s.handleError(w, http.StatusNotFound)
+		return
 	}
 
 	http.Redirect(w, r, srcURL, http.StatusFound)
-	return nil
+}
+
+func (s *Server) handleError(w http.ResponseWriter, code int) {
+	customError := struct {
+		Code int    `json:"code"`
+		Msg  string `json:"error"`
+	}{}
+
+	customError.Code = code
+
+	switch code {
+	case http.StatusNotFound:
+		customError.Msg = "Not found"
+	case http.StatusInternalServerError:
+		customError.Msg = "Internal server error"
+	case http.StatusBadRequest:
+		customError.Msg = "Bad request"
+	}
+
+	data, err := json.Marshal(&customError)
+	if err != nil {
+		s.handleError(w, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(data)
 }
 
 func makeOutputJSON(url string) ([]byte, error) {
